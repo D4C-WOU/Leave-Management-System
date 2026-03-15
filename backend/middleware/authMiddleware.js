8fd40c09c971136df19c64da6fa462e57d8b871e
D4C-WOU <nandj6154@gmail.com>
2026-03-15 22:12:39 +0530

    Removed console logs which may cause security breaches

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,11 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-  console.log("🔐 Protect middleware hit");
-
   const token = req.headers.authorization?.split(" ")[1];
-
-  console.log("Token received:", token);
 
   if (!token) {
     return res.status(401).json({ message: "Not authorized" });
@@ -13,8 +9,6 @@ const protect = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    console.log("Decoded user:", decoded);
 
     req.user = decoded;
 
