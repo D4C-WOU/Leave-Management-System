14d1ea4db4704a44b15cb54e5296dbfcec645b00
D4C-WOU <nandj6154@gmail.com>
2026-05-19 23:10:30 +0530

    Updated frontend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://leave-management-system-h12g.onrender.com",
 });
 
 // Attach token automatically
