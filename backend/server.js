18937668174b9ccc7b2b417433a21d4425e6cfde
D4C-WOU <nandj6154@gmail.com>
2026-05-19 23:05:54 +0530

    Added backend home route

--- backend/server.js
+++ backend/server.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("LeaveFlow Backend API is running...");
+});
